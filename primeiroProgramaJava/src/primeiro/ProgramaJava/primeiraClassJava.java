e516da410cd9624a613d2d9d5c31c8b040c9fd97
gusta <gusta@Gustavo>
2024-02-25 22:34:30 -0300

    Criação do projeto

---
+++ primeiroProgramaJava/src/primeiro/ProgramaJava/primeiraClassJava.java
@@ -0,0 +1,9 @@
+package primeiro.ProgramaJava;
+public class primeiraClassJava {
+
+		/*Main é um metodo executavel em java*/
+		public static void main(String[] args) {
+			/*Escreve texto no cosole*/
+			System.err.println("Meu primeiro programa em java"); 
+		}
+}
